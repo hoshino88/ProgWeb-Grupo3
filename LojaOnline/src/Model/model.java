f981398702b90bcc4b21f194f330ae4d75c5e09a
Alessandro Gabriel Pena Furtado <alessandrogpf@hotmail.com>
2024-10-22 12:53:32 -0400

    Pastas criadas e arquitetura MVC em andamento

---
+++ LojaOnline/src/Model/model.java
@@ -0,0 +1,8 @@
+package LojaOnline.src.Model;
+
+public class model {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
---
+++ LojaOnline/src/View/view.java
@@ -0,0 +1,8 @@
+package LojaOnline.src.View;
+
+public class view {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
